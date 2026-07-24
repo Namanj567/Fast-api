3592891ca1a2419a0678db7c35f7c7e11d267cf3
Namanj567 <namanjainnj567@gmail.com>
2026-07-24 13:24:07 +0530

    fast-api-docker

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Employee API Running"
+    }
+
+
+@app.get("/employees")
+def employees():
+    return [
+        {
+            "id":1,
+            "name":"Naman",
+            "department":"DevOps"
+        },
+        {
+            "id":2,
+            "name":"Rahul",
+            "department":"Cloud"
+        }
+    ]
